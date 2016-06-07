74ddff84375344de6cb30cf42722744eae1f2752
Kai Wohlfahrt <kai.scorpio@gmail.com>
2016-06-07 17:27:31 +0100

    Use struct to track callbacks

--- src/lib.rs
+++ src/lib.rs
@@ -4,17 +4,33 @@ mod c;
 use self::libc::{c_void, c_int};
 use std::mem;
 
+pub struct Callback<'a> {
+    #[allow(dead_code)] // used by C
+    callback: Box<Box<FnMut(i32) -> i32 + 'a>>,
+    c_handle: *const c::Callback,
+}
+
 extern fn callback_handler(x: c_int, arg: *mut c_void) -> c_int {
     let closure: &mut Box<FnMut(i32) -> i32> = unsafe { mem::transmute(arg) };
     closure(x as i32) as c_int
 }
 
-pub fn add_callback<F>(callback: F) -> *const c::Callback
-    where F: FnMut(i32) -> i32, F: 'static {
-    let cb: Box<Box<FnMut(i32) -> i32>> = Box::new(Box::new(callback));
-    let ptr = Box::into_raw(cb) as * mut _;
-    unsafe {
-        c::add_callback(Some(callback_handler), ptr)
+impl<'a> Callback<'a> {
+    pub fn new<F>(callback: F) -> Option<Self>
+        where F: FnMut(i32) -> i32 + 'a {
+        let callback: Box<Box<FnMut(i32) -> i32 + 'a>> = Box::new(Box::new(callback));
+        let ptr = &*callback as *const Box<_> as *mut c_void;
+
+        match unsafe{c::add_callback(Some(callback_handler), ptr).as_ref()} {
+            None => None,
+            Some(c_handle) => Some(Callback{callback: callback, c_handle: c_handle}),
+        }
+    }
+}
+
+impl<'a> Drop for Callback<'a> {
+    fn drop(&mut self) {
+        unsafe {c::remove_callback(self.c_handle)};
     }
 }
 
@@ -24,12 +40,6 @@ pub fn invoke_callbacks(x: i32) -> i32 {
     }
 }
 
-pub fn remove_callback(callback: *const c::Callback) {
-    let ptr = unsafe { c::remove_callback(callback) };
-    // drop the callback
-    let _: Box<Box<FnMut(i32) -> i32>> = unsafe { Box::from_raw(ptr as *mut _) };
-}
-
 #[cfg(test)]
 mod test {
     use super::*;
@@ -37,31 +47,31 @@ mod test {
     #[test]
     fn test_callback() {
         let mut y = 0;
-        let callback = add_callback(move |x| {
+        let callback = Callback::new(move |x| {
             y += 1;
             x - y
         });
 
         assert!(invoke_callbacks(2) == 1);
         assert!(invoke_callbacks(2) == 0);
-        remove_callback(callback);
+        drop(callback);
     }
 
     #[test]
     fn test_multiple_callbacks() {
         let mut y = 0;
-        let callback1 = add_callback(move |x| {
+        let callback1 = Callback::new(move |x| {
             y += 1;
             x - y
         });
-        let callback2 = add_callback(|x| {
+        let callback2 = Callback::new(|x| {
             x % 3
         });
 
         assert!(invoke_callbacks(2) == 3);
         assert!(invoke_callbacks(2) == 2);
-        remove_callback(callback1);
+        drop(callback1);
         assert!(invoke_callbacks(2) == 2);
-        remove_callback(callback2);
+        drop(callback2);
     }
 }
